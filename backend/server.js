e117b0a02ae1147f0f6334e953829224d66ac69c
michaelwijanto <michael.wijanto@gmail.com>
2024-06-19 17:16:38 +0700

    getUserProfile, followUnfollow, notification

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
-import auth from "./routes/auth.routes.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
+import auth from "./routes/auth.routes.js";
+import user from "./routes/user.routes.js";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ app.use(express.urlencoded({ extended: true })); //parse form data
 app.use(cookieParser()); //parse cookie from browser
 
 app.use("/api/auth", auth);
+app.use("/api/users", user);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
