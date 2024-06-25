271362689db0a21d3e1094eeb01c2e1c52d30311
michaelwijanto <michael.wijanto@gmail.com>
2024-06-25 15:59:48 +0700

    create delete post

---
+++ backend/controllers/post.controller.js
@@ -0,0 +1,62 @@
+import User from "../models/user.model.js";
+import Post from "../models/post.model.js";
+import { v2 as cloudinary } from "cloudinary";
+
+export const createPost = async (req, res) => {
+  try {
+    const { text } = req.body;
+    let { img } = req.body;
+    const userId = req.user._id.toString();
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (!text && !img) {
+      return res.status(400).json({ error: "Post must have text or image" });
+    }
+
+    if (img) {
+      const uploaded = await cloudinary.uploader.upload(img);
+      img = uploaded.secure_url;
+    }
+    const newPost = new Post({
+      user: userId,
+      text,
+      img,
+    });
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    console.log("Error in create post", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    if (post.user.toString() !== req.user._id.toString()) {
+      return res
+        .status(401)
+        .json({ erro: "You are not authorized to delete this post!" });
+    }
+
+    if (post.img) {
+      const imgId = post.img.split("/").pop().split(".")[0];
+      await cloudinary.uploader.destroy(imgId);
+    }
+
+    await Post.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({ message: "Post deleted succesfully!" });
+  } catch (error) {
+    console.log("Error in delete post", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 import auth from "./routes/auth.routes.js";
 import user from "./routes/user.routes.js";
-import { v2 as cloudinary } from "cloudinary";
+import post from "./routes/post.routes.js";
 
 dotenv.config();
 
@@ -23,6 +24,7 @@ app.use(cookieParser()); //parse cookie from browser
 
 app.use("/api/auth", auth);
 app.use("/api/users", user);
+app.use("/api/posts", post);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
