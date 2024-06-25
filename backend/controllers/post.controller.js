4bbce07f58f31fdf3d9168e1f2c02481de6f41a1
michaelwijanto <michael.wijanto@gmail.com>
2024-06-25 16:31:42 +0700

    comment post

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -60,3 +60,32 @@ export const deletePost = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+export const commentPost = async (req, res) => {
+  try {
+    const { text } = req.body;
+    const userId = req.user._id;
+
+    if (!text) {
+      return res.status(400).json({ error: "Text field is required!" });
+    }
+
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    const comment = {
+      user: userId,
+      text,
+    };
+
+    post.comments.push(comment);
+    await post.save();
+
+    res.status(200).json(post);
+  } catch (error) {
+    console.log("Error in comment post", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
