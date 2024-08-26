d5047e2292164532f5bd31a28ca5f1085e3b4c68
michaelwijanto <michael.wijanto@gmail.com>
2024-08-26 16:09:58 +0700

    add delete 1 notification

--- backend/controllers/notif.controller.js
+++ backend/controllers/notif.controller.js
@@ -29,3 +29,27 @@ export const deleteNotification = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+export const deleteOne = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userId = req.user._id;
+    const notif = await Notification.findById(id);
+
+    if (!notif) {
+      return res.status(404).json({ error: "Notification not found" });
+    }
+
+    if (Notification.to.toString() !== userId.toString()) {
+      return res
+        .status(403)
+        .json({ error: "You are not allowed to delete notification" });
+    }
+
+    await Notification.findByIdAndDelete(id);
+    res.status(200).json({ message: "Notification deleted successfully" });
+  } catch (error) {
+    console.log("Error in delete notification", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- backend/routes/notif.route.js
+++ backend/routes/notif.route.js
@@ -3,11 +3,13 @@ import { protectRoute } from "../middleware/protectRoute.js";
 import {
   getNotification,
   deleteNotification,
+  deleteOne,
 } from "../controllers/notif.controller.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getNotification);
 router.delete("/", protectRoute, deleteNotification);
+router.delete("/:id", protectRoute, deleteOne);
 
 export default router;
