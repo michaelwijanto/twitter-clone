ed1dd9cd6bb8c28ffbc7bff67e628074830f4aea
michaelwijanto <michael.wijanto@gmail.com>
2024-08-28 15:47:54 +0700

    logout

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -6,7 +6,32 @@ import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
 
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+
 const Sidebar = () => {
+  const logout = "/api/auth/logout";
+
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch(logout, {
+          method: "POST",
+        });
+
+        const data = await res.json();
+
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    onSuccess: () => {
+      toast.success("Logout successful");
+    },
+  });
   const data = {
     fullName: "John Doe",
     username: "johndoe",
@@ -66,7 +91,13 @@ const Sidebar = () => {
                 </p>
                 <p className="text-slate-500 text-sm">@{data?.username}</p>
               </div>
-              <BiLogOut className="w-5 h-5 cursor-pointer" />
+              <BiLogOut
+                className="w-5 h-5 cursor-pointer"
+                onClick={(e) => {
+                  e.preventDefault();
+                  mutate();
+                }}
+              />
             </div>
           </Link>
         )}
