72edf94e4a57c1235bd8af1cde906a3a27e29566
michaelwijanto <michael.wijanto@gmail.com>
2024-08-28 15:15:20 +0700

    connect signup with db and toast

--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -7,6 +7,7 @@ import { MdOutlineMail } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
 import { MdPassword } from "react-icons/md";
 import { MdDriveFileRenameOutline } from "react-icons/md";
+import { useMutation } from "@tanstack/react-query";
 
 const SignUpPage = () => {
   const [formData, setFormData] = useState({
@@ -15,17 +16,45 @@ const SignUpPage = () => {
     fullName: "",
     password: "",
   });
+  const signup = "/api/auth/signup";
 
+  const { mutate, isError, isPending, error } = useMutation({
+    mutationFn: async ({ email, username, fullName, password }) => {
+      try {
+        const res = await fetch(signup, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, username, fullName, password }),
+        });
+
+        const data = await res.json();
+        if (!res.ok)
+          throw new Error(data.Error || "Failed to create an account");
+        // console.log(data);
+        setFormData({ email: "", username: "", fullName: "", password: "" });
+        return data;
+      } catch (error) {
+        // console.log(error);
+        throw error;
+      }
+    },
+    onSuccess: () => {
+      toast.success("Account created succesfully");
+    },
+  });
   const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
+    e.preventDefault(); //page won't reload
+    mutate(formData);
+    // console.log(formData);
   };
 
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const isError = false;
+  // const isError = false;
 
   return (
     <div className="max-w-screen-xl mx-auto flex h-screen px-10">
@@ -86,9 +115,13 @@ const SignUpPage = () => {
             />
           </label>
           <button className="btn rounded-full btn-primary text-white">
-            Sign up
+            {isPending ? (
+              <span className="loading loading-spinner loading-xs"></span>
+            ) : (
+              "Sign up"
+            )}
           </button>
-          {isError && <p className="text-red-500">Something went wrong</p>}
+          {isError && <p className="text-red-500">{error.message}</p>}
         </form>
         <div className="flex flex-col lg:w-2/3 gap-2 mt-4">
           <p className="text-white text-lg">Already have an account?</p>
