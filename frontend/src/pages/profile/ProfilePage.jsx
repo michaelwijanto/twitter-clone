9ba0e6da54be41708bc91dffcafe07f1a7f302b6
michaelwijanto <michael.wijanto@gmail.com>
2024-08-30 14:13:50 +0700

    fix promise and update user profile bio

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -52,7 +52,7 @@ const ProfilePage = () => {
   const { mutate: updateProfile, isPending: isUpdatingProfile } = useMutation({
     mutationFn: async () => {
       try {
-        const res = await fetch("/api/users/update", {
+        const res = await fetch(`/api/users/update`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -72,8 +72,8 @@ const ProfilePage = () => {
     onSuccess: () => {
       toast.success("Profile updated successfully");
       Promise.all([
-        (queryClient.invalidateQueries = { queryKey: ["authUser"] }),
-        (queryClient.invalidateQueries = { queryKey: ["userProfile"] }),
+        queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+        queryClient.invalidateQueries({ queryKey: ["userProfile"] }),
       ]);
     },
     onError: (error) => {
@@ -173,7 +173,7 @@ const ProfilePage = () => {
                 </div>
               </div>
               <div className="flex justify-end px-4 mt-5">
-                {isMyProfile && <EditProfileModal />}
+                {isMyProfile && <EditProfileModal authUser={authUser} />}
                 {!isMyProfile && (
                   <button
                     className="btn btn-outline rounded-full btn-sm"
@@ -209,12 +209,12 @@ const ProfilePage = () => {
                       <>
                         <FaLink className="w-3 h-3 text-slate-500" />
                         <a
-                          href="https://youtube.com/@asaprogrammer_"
+                          href={user?.link}
                           target="_blank"
                           rel="noreferrer"
                           className="text-sm text-blue-500 hover:underline"
                         >
-                          youtube.com/@asaprogrammer_
+                          {user?.link}
                         </a>
                       </>
                     </div>
